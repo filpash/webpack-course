d5b6618fbafbdab125a9c154db919a5ad49e2a56
Filipp <fili92@mail.ru>
2020-07-08 20:44:39 +0300

    conneting by library & optimization

--- src/analytic.js
+++ src/analytic.js
@@ -1,3 +1,5 @@
+import * as $ from 'jquery'
+
 function createAnalytics() {
     let counter = 0
     let destroyed = false
@@ -6,11 +8,11 @@ function createAnalytics() {
 
     const listener = () => counter++
 
-    document.addEventListener('click', listener)
+    $(document).on('click', listener)
 
     return {
         destroy() {
-            document.removeEventListener('click', listener)
+            $(document).off('click', listener)
             destroyed = true
         },
 
